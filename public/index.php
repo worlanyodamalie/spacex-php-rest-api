40ad9d227fa867db0c71f5ae7ea0e341201ac2f7
Worlanyo <worladamalie@gmail.com>
2023-07-25 06:06:09 +0000

    Inplement JWT authorization for API

--- public/index.php
+++ public/index.php
@@ -1,16 +1,36 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+//header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    //header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header("Access-Control-Allow-Credentials: true");
+    exit;
+}
+// header("Access-Control-Allow-Origin: *");
+// header("Content-Type: application/json; charset=UTF-8");
+// header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+// header('Access-Control-Max-Age: 86400');
+// header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// header("HTTP/1.0 200 OK");
+
+
 require "../bootstrap.php";
 use Src\SpaceXController;
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $uriParts = explode( '/', $uri );
+
+$controller = new SpaceXController();
 
 $routes = [
     'capsules' => [
@@ -31,6 +51,11 @@ $routes = [
         'controller_method' => 'rockets'
 
     ],
+    'generateToken' => [
+        'method' => 'GET',
+        'expression' => '/^\/generateToken\/?$/',
+        'controller_method' => 'generateToken'
+    ]
     ];
 
 $routeFound = null;
@@ -42,20 +67,62 @@ foreach ($routes as $route) {
             $routeFound = $route;
             break;
         }
-    }  
+}  
     
-    if (! $routeFound) {
-        header("HTTP/1.1 404 Not Found");
-        exit();
-    }
+if (! $routeFound) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
     
-    $methodName = $route['controller_method'];
+$methodName = $route['controller_method'];
 
     // $body = null;
 
     // if(isset($uri[2])){
     //     $body = $_SERVER;
     // }
+if($methodName != 'generateToken'){
+    if(! authenticate($methodName)){
+        header("HTTP/1.1 401 Unauthorized");
+        exit("Unauthorized");
+    }
+}
+    
+// if(! authenticate($methodName)){
+//     header("HTTP/1.1 401 Unauthorized");
+//     exit("Unauthorized");
+// }
 
+    
+$controller->$methodName($uri);
+
+function authenticate($methodName){
+    //print_r($_SERVER);
     $controller = new SpaceXController();
-    $controller->$methodName($uri);
+    if(! isset($_SERVER['HTTP_AUTHORIZATION']) ){
+           return false;
+       }
+
+    preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'] , $matches );
+
+    if(! isset($matches[1])) {
+          return false;
+       }
+
+
+    $token = $matches[1];
+
+    $tokenSections = explode('.',$token);
+
+    $decoded_token =  $controller->decode($token);
+
+    // print_r($tokenSections);
+    // echo "Inside index";
+    // print_r($decoded_token);
+
+    if(! $decoded_token->iss === $_ENV['TOKEN_ISSUER']){
+          return false;
+    }
+
+    return true;
+    }
--- src/SpaceXController.php
+++ src/SpaceXController.php
@@ -1,38 +1,104 @@
 <?php
 namespace Src;
-
+require "../bootstrap.php";
 use Psr\Http\Message\ResponseInterface;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class SpaceXController {
 
     private $client = null;
-    // private $body;
+    private $secret;
+    private $issuedAt ;
+    private $expire ;
 
     public function __construct(){
         $this->client = new Client([
             'base_uri' => 'https://api.spacexdata.com/v4/'
         ]);
 
+        date_default_timezone_set('Africa/Accra');
+        $this->issuedAt = time();
+        $this->expire = $this->issuedAt + 3600;
+        $this->secret = "secret_used_for_test_not_for_production";
+
         // $this->body = $body;
+    }
+
+    public function encode($iss, $data)
+    {
+        // echo($this->secrect);
+
+        $token = array(
+            "iss" => $iss,
+            "aud" => $iss,
+            "iat" => $this->issuedAt,
+            "exp" => $this->expire,
+            "data" => $data
+        );
+
+        return JWT::encode($token, $this->secret, 'HS256');
+    }
+
+    public function decode($token)
+    {
+        try {
+            $decode = JWT::decode($token, new Key($this->secret, 'HS256'));
+            // echo "Inside decode";
+            // print_r($decode);
+            return $decode;
+        } catch (Exception $e) {
+            
+            return $e->getMessage();
+        }
+    }
+
+    public function generateToken(){
+        header("Content-Type: application/json");
+        $payload = "token payload,this should not be used in production";
+        $iss = $_ENV['TOKEN_ISSUER'];
+        $token = $this->encode($iss , $payload);
+        
+        
+        $response = array();
+        $response["token"] = $token; 
+       
+        $data = json_encode($response,true);
+        echo $data;
+        return $data;
     }
 
 
     public function capsules($uri){
+        $body = null;
+        if(is_object(file_get_contents("php://input"))){
+            $body = file_get_contents("php://input");
+        }
         
-        $body = file_get_contents("php://input");
+        //echo $body;
         $headers = [
-            'Content-Type' => 'application/json'
+            'Content-Type' => 'application/json; charset=UTF-8',
+            // 'Access-Control-Allow-Origin' =>  '*',
+            // "Access-Control-Allow-Methods" => "OPTIONS,GET,POST,PUT,DELETE",
+            // "Access-Control-Max-Age" => "86400",
+            // "Access-Control-Allow-Headers" => "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With" ,
+            // "HTTP/1.0 200 OK"
           ];
-        //print_r($body);
+       //print_r($body);
+       //return  header("HTTP/1.0 200 OK");
+       
 
         try {
             $request = new Request('POST', 'capsules/query' , $headers ,$body);
             $res = $this->client->sendAsync($request)->wait();
-            //echo $res->getBody();
-            return $res->getBody(); 
+            
+           
+            echo $res->getBody();
+            return $res->getBody();
+           
         } catch (RequestException $e) {
            echo $e;
         }
@@ -43,14 +109,14 @@ class SpaceXController {
        
         // print_r($this->body);
         
-        // try {
-        //     $request = new Request('GET', 'rockets');
-        //     $res = $this->client->sendAsync($request)->wait();
-        //     echo $res->getBody();
-        //     return $res->getBody(); 
-        // } catch (RequestException $e) {
-        //    echo $e;
-        // }
+        try {
+            $request = new Request('GET', 'rockets');
+            $res = $this->client->sendAsync($request)->wait();
+            echo $res->getBody();
+            return $res->getBody(); 
+        } catch (RequestException $e) {
+           echo $e;
+        }
        
     }
 }
